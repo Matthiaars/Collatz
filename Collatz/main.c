6ff102a0a175dd229cf95cff9b94334b277c0f28
Matthias Lemmens <Matthias@p082h233.private.ugent.be>
2011-11-28 12:28:29 +0100

    Initial Commit

---
+++ Collatz/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Collatz
+//
+//  Created by Matthias Lemmens on 28/11/11.
+//  Copyright (c) 2011 Lemmens inc. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
