91a02dafcb6fce26f35f8c2ac56f653ba5d59ac6
Matthiaars <matthias.lemmens@gmail.com>
2012-05-27 16:46:26 +0200

    I have no idea what I'm doing

--- Collatz/main.c
+++ Collatz/main.c
@@ -8,11 +8,40 @@
 
 #include <stdio.h>
 
+void verwerk(int* getalp, int* counterp)
+{ 
+    // Nodige bewerking op uitvoeren
+    
+    if ((*getalp % 2) == 0) {
+        *getalp = *getalp / 2;
+    } else {
+        *getalp = (3 * *getalp) + 1;
+    }
+    
+    // Teller + 1 (begint bij 0, dus optellen voor afdrukken)
+    
+    *counterp += 1;
+
+    // Teller en getal afdrukken
+    
+    printf("%d: %d \n", *counterp, *getalp);
+    
+    // Verdergaan of eindigen
+    
+    if (*getalp != 1) {
+        verwerk(getalp, counterp);
+    } else {
+        printf("There were %d steps in the process. You're welcome.\n", *counterp);
+    }
+}
+
 int main (int argc, const char * argv[])
 {
-
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+    int getal, counter;
+    
+    printf("Hello ladies! Give me a number, and I'll show you it's Collatz conjecture all night long...:");
+    scanf("%d", &getal);
+    
+    verwerk(&getal, &counter);
 }
 
